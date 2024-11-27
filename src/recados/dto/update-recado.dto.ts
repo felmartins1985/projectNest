9bcf04e1bd361e3c8b3437b5fc15754faec76e8e
Felipe Costa Martins <felmartins1985@gmail.com>
2024-11-27 15:25:50 -0300

    feat:  install mapped-types

--- src/recados/dto/update-recado.dto.ts
+++ src/recados/dto/update-recado.dto.ts
@@ -1,5 +1,10 @@
-export class UpdateRecadoDto {
-  readonly texto?: string;
-  readonly de?: string;
-  readonly para?: string;
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateRecadoDto } from './create-recado.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+
+export class UpdateRecadoDto extends PartialType(CreateRecadoDto) {
+  // eu posso estender a classe e tambem posso criar ou atualizar coisas dentro do campo
+  @IsBoolean()
+  @IsOptional()
+  readonly lido: boolean;
 }
