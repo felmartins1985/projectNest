e094c4dee078f490632dbefd4a255815af932b5b
Felipe Costa Martins <felmartins1985@gmail.com>
2024-11-28 15:10:02 -0300

    feat: create the relationship

--- src/pessoas/entities/pessoa.entity.ts
+++ src/pessoas/entities/pessoa.entity.ts
@@ -1,8 +1,10 @@
 import { IsEmail } from 'class-validator';
+import { Recado } from 'src/recados/entities/recado.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -22,4 +24,13 @@ export class Pessoa {
   createdAt?: Date;
   @UpdateDateColumn()
   updatedAt?: Date;
+
+  // uma pessoa pode ter enviados muitos recados com o seu ID atrelado em "de"
+  @OneToMany(() => Recado, recado => recado.de)
+  // esses recados sao relacionados ao campo "de" na entidade recado
+  recadosEnviados: Recado[];
+  // uma pessoa pode ter recebido muitos recados com o seu ID atrelado em "para"
+  // esses recados sao relacionados ao campo "para" na entidade recado
+  @OneToMany(() => Recado, recado => recado.para)
+  recadosRecebidos: Recado[];
 }
--- src/recados/entities/recado.entity.ts
+++ src/recados/entities/recado.entity.ts
@@ -1,7 +1,10 @@
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -12,16 +15,28 @@ export class Recado {
   id: number;
   @Column({ type: 'varchar', length: 255 })
   texto: string;
-  @Column({ type: 'varchar', length: 50 })
-  de: string;
-  @Column({ type: 'varchar', length: 50 })
-  para: string;
+
   @Column({ default: false })
   lido: boolean;
+
   @Column()
   data: Date;
+
   @CreateDateColumn()
   createdAt?: Date;
+
   @UpdateDateColumn()
   updatedAt?: Date;
+
+  // muitos recados podem ser enviados por uma unica pessoa(emissor)
+  @ManyToOne(() => Pessoa)
+  // especifica a coluna "de" que armazena o id da pessoa que enviou o recado
+  @JoinColumn({ name: 'de' })
+  de: Pessoa;
+
+  // muitos recados podem ser enviados para uma unica pessoa(destinatario)
+  @ManyToOne(() => Pessoa)
+  // especifica a coluna "para" que armazena o id da pessoa que recebeu o recado
+  @JoinColumn({ name: 'para' })
+  para: Pessoa;
 }
