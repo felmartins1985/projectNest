bf597335db9b67485b57364a1b2bc300ce6ef9c1
Felipe Costa Martins <felmartins1985@gmail.com>
2024-11-28 11:15:39 -0300

    feat: install dbeaver and postgres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 //
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    RecadosModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: 'gemeos1985',
+      autoLoadEntities: true, // carrega as entidades automaticamente sem precisar especificar
+      synchronize: true, // sincroniza com o BD. Nao deve ser usado em producao
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
