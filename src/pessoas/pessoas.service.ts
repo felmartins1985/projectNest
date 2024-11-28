b84cd6c20f24cfcd2f50e793eca349f6a946d6ac
Felipe Costa Martins <felmartins1985@gmail.com>
2024-11-28 14:35:25 -0300

    feat: delete and find person method

--- src/pessoas/pessoas.service.ts
+++ src/pessoas/pessoas.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -29,8 +33,13 @@ export class PessoasService {
     }
   }
 
-  findAll() {
-    return `This action returns all pessoas`;
+  async findAll() {
+    const pessoas = await this.pessoaRepository.find({
+      order: {
+        id: 'DESC',
+      },
+    });
+    return pessoas;
   }
 
   findOne(id: number) {
@@ -41,7 +50,11 @@ export class PessoasService {
     return `This action updates a #${id} pessoa`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pessoa`;
+  async remove(id: number) {
+    const pessoa = await this.pessoaRepository.findOneBy({ id });
+    if (!pessoa) {
+      throw new NotFoundException('Pessoa não encontrada');
+    }
+    return await this.pessoaRepository.remove(pessoa);
   }
 }
