55401ae8e6b8f361efe3900bf8323202d73e5cba
Felipe Costa Martins <felmartins1985@gmail.com>
2024-12-03 14:23:23 -0300

    create a function on app.config that will return the variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,14 @@ import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
-//
+import appConfig from './app.config';
 
 @Module({
-  // carrega um arquivo em que serao salvas a variaveis de ambiente
-  //   ConfigModule.forRoot({}) ==> pega o .env na raiz do projeto
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env', // caso o arquivo tenha outro nome ou nao esteja na raiz do projeto
+      load: [appConfig],
       validationSchema: Joi.object({
         DATABASE_TYPE: Joi.required(),
         DATABASE_HOST: Joi.required(),
@@ -25,43 +23,29 @@ import * as Joi from '@hapi/joi';
         DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0),
       }),
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: configService.get<'postgres'>('database.type'),
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          username: configService.get<string>('database.username'),
+          database: configService.get<string>('database.database'),
+          password: configService.get<string>('database.password'),
+          autoLoadEntities: configService.get<boolean>(
+            'database.autoLoadEntities',
+          ),
+          synchronize: configService.get<boolean>('database.synchronize'),
+        };
+      },
+    }),
     RecadosModule,
     PessoasModule,
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      database: process.env.DATABASE_DATABASE,
-      password: process.env.DATABASE_PASSWORD,
-      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // carrega as entidades automaticamente sem precisar especificar
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // sincroniza com o BD. Nao deve ser usado em producao
-    }),
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    // {
-    //   provide: APP_FILTER,
-    //   useClass: MyExceptionFilter,
-    // }, //uma forma de utilizar sem ser colocar no main.ts New MyExceptionFilter()
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: IsAdminGuard,
-    // },
-  ],
+  providers: [AppService],
+  exports: [],
 })
 export class AppModule {}
-// export class AppModule implements NestModule {
-//   // implementacao para funcionar os middlewares -->  A ORDEM IMPORTA DOS MIDDLEWARES
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer.apply(SimpleMiddleware).forRoutes({
-//       path: '*',
-//       method: RequestMethod.ALL,
-//     });
-//     // consumer.apply(OutroMiddleware).forRoutes({
-//     //   path: '*',
-//     //   method: RequestMethod.ALL,
-//     // });
-//   }
-// }
