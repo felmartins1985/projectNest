1f30e531d8826e2356955c421c8f0b972e18f1b9
Felipe Costa Martins <felmartins1985@gmail.com>
2024-12-06 10:55:38 -0300

    feat: insert validator on the upload

--- src/pessoas/pessoas.controller.ts
+++ src/pessoas/pessoas.controller.ts
@@ -9,7 +9,9 @@ import {
   UseGuards,
   UseInterceptors,
   UploadedFile,
-  UploadedFiles,
+  ParseFilePipeBuilder,
+  HttpStatus,
+  BadRequestException,
 } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
@@ -17,10 +19,10 @@ import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPayloadParam } from 'src/auth/params/token-payload.params';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor } from '@nestjs/platform-express';
 import * as path from 'path';
 import * as fs from 'fs/promises';
-import { randomUUID } from 'crypto';
+
 @Controller('pessoas')
 export class PessoasController {
   constructor(private readonly pessoasService: PessoasService) {}
@@ -58,41 +60,43 @@ export class PessoasController {
   }
 
   @UseGuards(AuthTokenGuard)
-  @UseInterceptors(FilesInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file'))
   @Post('upload-picture')
   async uploadPicture(
-    @UploadedFiles() files: Array<Express.Multer.File>,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /jpeg|jpg|png/g,
+        })
+        .addMaxSizeValidator({
+          maxSize: 10 * (1024 * 1024),
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
     @TokenPayloadParam() tokenPayload: TokenPayloadDto,
   ) {
-    const result = [];
-    files.forEach(async file => {
-      const fileExtension = path
-        .extname(file.originalname)
-        .toLowerCase()
-        .substring(1); // pega a extensao do arquivo e retira o ponto
-      // console.log(fileExtension);
-      const fileName = `${randomUUID()}.${fileExtension}`;
-      const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
-      result.push(fileFullPath);
-      await fs.writeFile(fileFullPath, file.buffer);
-    });
-    return result;
-    // const fileExtension = path
-    //   .extname(file.originalname)
-    //   .toLowerCase()
-    //   .substring(1); // pega a extensao do arquivo e retira o ponto
-    // // console.log(fileExtension);
-    // const fileName = `${tokenPayload.sub}.${fileExtension}`;
-    // const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
-    // console.log(fileFullPath);
-    // await fs.writeFile(fileFullPath, file.buffer);
-    // return {
-    //   fieldname: file.fieldname,
-    //   originalname: file.originalname,
-    //   encoding: file.encoding,
-    //   mimetype: file.mimetype,
-    //   size: file.size,
-    //   buffer: {},
-    // };
+    if (file.size < 1024) {
+      throw new BadRequestException('O arquivo deve ter no mínimo 1KB');
+    }
+    const fileExtension = path
+      .extname(file.originalname)
+      .toLowerCase()
+      .substring(1); // pega a extensao do arquivo e retira o ponto
+    // console.log(fileExtension);
+    const fileName = `${tokenPayload.sub}.${fileExtension}`;
+    const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
+    console.log(fileFullPath);
+    await fs.writeFile(fileFullPath, file.buffer);
+    return {
+      fieldname: file.fieldname,
+      originalname: file.originalname,
+      encoding: file.encoding,
+      mimetype: file.mimetype,
+      size: file.size,
+      buffer: {},
+    };
   }
 }
