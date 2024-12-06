9651dd40d2fbd8a6ed39cfb0291cc40ef870d214
Felipe Costa Martins <felmartins1985@gmail.com>
2024-12-06 10:32:39 -0300

    feat: send more than 1 picture to the server

--- src/pessoas/pessoas.controller.ts
+++ src/pessoas/pessoas.controller.ts
@@ -9,6 +9,7 @@ import {
   UseGuards,
   UseInterceptors,
   UploadedFile,
+  UploadedFiles,
 } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
@@ -16,9 +17,10 @@ import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPayloadParam } from 'src/auth/params/token-payload.params';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import * as path from 'path';
 import * as fs from 'fs/promises';
+import { randomUUID } from 'crypto';
 @Controller('pessoas')
 export class PessoasController {
   constructor(private readonly pessoasService: PessoasService) {}
@@ -56,28 +58,41 @@ export class PessoasController {
   }
 
   @UseGuards(AuthTokenGuard)
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FilesInterceptor('file'))
   @Post('upload-picture')
   async uploadPicture(
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFiles() files: Array<Express.Multer.File>,
     @TokenPayloadParam() tokenPayload: TokenPayloadDto,
   ) {
-    const fileExtension = path
-      .extname(file.originalname)
-      .toLowerCase()
-      .substring(1); // pega a extensao do arquivo e retira o ponto
-    // console.log(fileExtension);
-    const fileName = `${tokenPayload.sub}.${fileExtension}`;
-    const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
-    console.log(fileFullPath);
-    await fs.writeFile(fileFullPath, file.buffer);
-    return {
-      fieldname: file.fieldname,
-      originalname: file.originalname,
-      encoding: file.encoding,
-      mimetype: file.mimetype,
-      size: file.size,
-      buffer: {},
-    };
+    const result = [];
+    files.forEach(async file => {
+      const fileExtension = path
+        .extname(file.originalname)
+        .toLowerCase()
+        .substring(1); // pega a extensao do arquivo e retira o ponto
+      // console.log(fileExtension);
+      const fileName = `${randomUUID()}.${fileExtension}`;
+      const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
+      result.push(fileFullPath);
+      await fs.writeFile(fileFullPath, file.buffer);
+    });
+    return result;
+    // const fileExtension = path
+    //   .extname(file.originalname)
+    //   .toLowerCase()
+    //   .substring(1); // pega a extensao do arquivo e retira o ponto
+    // // console.log(fileExtension);
+    // const fileName = `${tokenPayload.sub}.${fileExtension}`;
+    // const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
+    // console.log(fileFullPath);
+    // await fs.writeFile(fileFullPath, file.buffer);
+    // return {
+    //   fieldname: file.fieldname,
+    //   originalname: file.originalname,
+    //   encoding: file.encoding,
+    //   mimetype: file.mimetype,
+    //   size: file.size,
+    //   buffer: {},
+    // };
   }
 }
