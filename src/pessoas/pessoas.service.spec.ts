c68a7de3e22f24389245392534a6f2349f06eef0
Felipe Costa Martins <felmartins1985@gmail.com>
2024-12-06 14:38:53 -0300

    feat: create first pessoa test

---
+++ src/pessoas/pessoas.service.spec.ts
@@ -0,0 +1,38 @@
+import { Repository } from 'typeorm';
+import { PessoasService } from './pessoas.service';
+import { Pessoa } from './entities/pessoa.entity';
+import { HashingService } from 'src/auth/hashing/hashing.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+describe('PessoasService', () => {
+  let pessoaService: PessoasService;
+  let pessoaRepository: Repository<Pessoa>;
+  let hashingService: HashingService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PessoasService,
+        {
+          provide: getRepositoryToken(Pessoa),
+          useValue: {},
+        },
+        {
+          provide: HashingService,
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    pessoaService = await module.resolve<PessoasService>(PessoasService);
+    pessoaRepository = module.get<Repository<Pessoa>>(
+      getRepositoryToken(Pessoa),
+    ); 
+    hashingService = module.get<HashingService>(HashingService);
+  });
+
+  it('pessoaService deve estar definido', () => {
+    expect(pessoaService).toBeDefined();
+  });
+});
