c2134eff40fd4c7ebd7cca97ae06174b7c0d4497
Felipe Costa Martins <felmartins1985@gmail.com>
2024-12-09 16:39:32 -0300

    feat: initiate e2e tests

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ParseIntIdPipe } from './common/pipes/parse-int-id.pipe';
+import appConfig from './app/config/app.config';
 // import { MyExceptionFilter } from './common/exceptions/my-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // remove as chaves que nao estao presentes nas validações
-    forbidNonWhitelisted: true, // retorna um erro se tiver uma chave que nao esta nas validações
-    transform: false, // tenta transformar os tipos de dados de dados para as validações
+  appConfig(app);
 
-  }),
-  new ParseIntIdPipe() // dessa forma funciona globalmente para todas as rotas
-  // se eu quiser colocar para um controller especifico, posso ir ao controller e colocar @UsePipes(ParseIntIdPipe) antes da classe
-);
+  await app.listen(3000);
   // app.useGlobalFilters(new MyExceptionFilter())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-// Definir transform como true no 
-// ValidationPipe é útil para garantir que os dados de 
-// entrada sejam automaticamente convertidos para os tipos esperados,
-// facilitando a validação e o processamento dos dados no seu aplicativo NestJS.
